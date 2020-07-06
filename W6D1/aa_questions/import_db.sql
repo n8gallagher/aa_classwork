92b5a825c3f18e01073a52573540960429f6ee6c
nateg <n8gallagher@gmail.com>
2020-07-06 11:45:20 -0700

    added replies table, question_likes table, and started seeding the database

--- W6D1/aa_questions/import_db.sql
+++ W6D1/aa_questions/import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
@@ -12,7 +14,6 @@ CREATE TABLE  questions (
     user_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
-
 )
 
 CREATE TABLE  question_follows (
@@ -29,9 +30,42 @@ CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
     body TEXT NOT NULL,
     user_id INTEGER NOT NULL,
-    subject_question TEXT NOT NULL,
-    
-
+    subject_question_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
 
     FOREIGN KEY user_id REFERENCES users(id),
+    FOREIGN KEY subject_question_id REFERENCES questions(id),
+    FOREIGN KEY parent_reply_id REFERENCES replies(id)
 )
+
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY user_id REFERENCES users(id),
+    FOREIGN KEY question_id REFERENCES questions(id)
+)
+
+INSERT INTO
+    users (fname, lname)
+VALUES
+    ('Nate', 'Gallagher')
+    ('Harry', 'Potter')
+    ('John', 'Doe')
+
+INSERT INTO
+    questions (title, body, user_id)
+VALUES
+    ('Help', 'what is a computer?', 2)
+    ('anon', 'seed question', 3)
+    ('rails', 'Is this rails?', 1)
+
+INSERT INTO
+    replies (body, user_id, subject_question_id, parent_reply_id)
+VALUES
+    ('No, this is not rails, there is no magic', 2, 3)
+    ('Wrong, there is magic', 3, 3, 1)
+    ('What is seed?', 1, 2)
+    ('A computer is not a bot', 3, 1)
+
