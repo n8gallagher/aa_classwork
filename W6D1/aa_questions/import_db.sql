0de50d624399c2dea2ec2b7869170d400a6e8f08
nateg <n8gallagher@gmail.com>
2020-07-06 11:59:07 -0700

    finished seeding database and debugged syntax errors; created the db

--- W6D1/aa_questions/import_db.sql
+++ W6D1/aa_questions/import_db.sql
@@ -3,9 +3,9 @@ PRAGMA foreign_keys = ON;
 CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL,
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 
-)
+);
 
 CREATE TABLE  questions (
     id INTEGER PRIMARY KEY,
@@ -14,17 +14,17 @@ CREATE TABLE  questions (
     user_id INTEGER NOT NULL,
 
     FOREIGN KEY (user_id) REFERENCES users(id)
-)
+);
 
 CREATE TABLE  question_follows (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
 
-    FOREIGN KEY question_id REFERENCES questions(id),
-    FOREIGN KEY user_id REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 
-)
+);
 
 CREATE TABLE replies (
     id INTEGER PRIMARY KEY,
@@ -33,39 +33,57 @@ CREATE TABLE replies (
     subject_question_id INTEGER NOT NULL,
     parent_reply_id INTEGER,
 
-    FOREIGN KEY user_id REFERENCES users(id),
-    FOREIGN KEY subject_question_id REFERENCES questions(id),
-    FOREIGN KEY parent_reply_id REFERENCES replies(id)
-)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (subject_question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent_reply_id) REFERENCES replies(id)
+);
 
 CREATE TABLE question_likes (
     id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
 
-    FOREIGN KEY user_id REFERENCES users(id),
-    FOREIGN KEY question_id REFERENCES questions(id)
-)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
 
 INSERT INTO
     users (fname, lname)
 VALUES
-    ('Nate', 'Gallagher')
-    ('Harry', 'Potter')
-    ('John', 'Doe')
+    ('Nate', 'Gallagher'),
+    ('Harry', 'Potter'),
+    ('John', 'Doe');
 
 INSERT INTO
     questions (title, body, user_id)
 VALUES
-    ('Help', 'what is a computer?', 2)
-    ('anon', 'seed question', 3)
-    ('rails', 'Is this rails?', 1)
+    ('Help', 'what is a computer?', 2),
+    ('anon', 'seed question', 3),
+    ('rails', 'Is this rails?', 1);
 
 INSERT INTO
-    replies (body, user_id, subject_question_id, parent_reply_id)
+    replies (body, user_id, subject_question_id)
 VALUES
-    ('No, this is not rails, there is no magic', 2, 3)
-    ('Wrong, there is magic', 3, 3, 1)
-    ('What is seed?', 1, 2)
-    ('A computer is not a bot', 3, 1)
+    ('No, this is not rails, there is no magic', 2, 3),
+    ('What is seed?', 1, 2),
+    ('A computer is not a bot', 3, 1);
 
+INSERT INTO
+    question_follows (user_id, question_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (2, 3),
+    (3, 1),
+    (3, 2);
+
+INSERT INTO
+    question_likes (user_id, question_id)
+VALUES
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (3, 1),
+    (3, 2),
+    (3, 3);
