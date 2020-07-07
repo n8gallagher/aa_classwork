115f9e59318574f5928513ebda7d456243c1de0f
nateg <n8gallagher@gmail.com>
2020-07-07 16:06:50 -0700

    progress on the metaprogramming project

--- W6D2/activerecord_lite/lib/00_attr_accessor_object.rb
+++ W6D2/activerecord_lite/lib/00_attr_accessor_object.rb
@@ -1,5 +1,15 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method(name) do
+        "value of @#{name}"
+        
+      end
+
+      define_method("#{name}=") do
+        
+      end
+
+    end
   end
 end
