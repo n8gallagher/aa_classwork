7552fb66f8f0e62fee13e496df267572e27f78a3
nateg <n8gallagher@gmail.com>
2020-07-08 17:05:11 -0700

    progress on Art Shares Project

---
+++ W6D3/first_routes/db/migrate/20200708230756_add_indexes_and_unique_artworks.rb
@@ -0,0 +1,7 @@
+class AddIndexesAndUniqueArtworks < ActiveRecord::Migration[5.2]
+  def change
+    add_index :artworks, [:title, :artist_id], unique: true
+    add_index :artworks, :artist_id
+
+  end
+end
---
+++ W6D3/first_routes/db/seeds.rb
@@ -0,0 +1,16 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+mike = User.create([{username: 'Mean Mike'}])
+vanelle = User.create([{username: 'Not Michelle'}])
+
+hair = Artwork.create([{artist_id: 1, title: 'mike\'s hair', image_url: 'hair.png'}])
+aa_logo = Artwork.create([{artist_id: 2, title: 'app academy logo', image_url: 'aa_logo.png'}])
+
+as1 = ArtworkShare.create([{artwork_id: 1, viewer_id: 2}])
+as2 = ArtworkShare.create([{artwork_id: 2, viewer_id: 1}])
