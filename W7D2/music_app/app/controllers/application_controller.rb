8f886dd30cbc0024d98a01507d20c1f4b397e07a
nateg <n8gallagher@gmail.com>
2020-07-14 16:59:54 -0700

    progress on music app

--- W7D2/music_app/app/controllers/application_controller.rb
+++ W7D2/music_app/app/controllers/application_controller.rb
@@ -10,6 +10,10 @@ class ApplicationController < ActionController::Base
         redirect_to new_session_url if !logged_in?
     end
 
+    def require_logged_out
+        redirect_to users_url if logged_in?
+    end
+
     def login!(user)
         session[:session_token] = user.reset_session_token!
     end
--- W7D2/music_app/app/controllers/sessions_controller.rb
+++ W7D2/music_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find_by_credentials(params[:email], params[:password])
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
         if @user && @user.is_password?(params[:password])
             login!(@user)
             redirect_to user_url(@user)
@@ -15,8 +15,9 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        @user = User.find_by(session_token: session[:session_token])
-        logout!(@user)
+        if logged_in?
+            logout!
+        end
         redirect_to users_url
     end
 end
